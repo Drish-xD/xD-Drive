f021defe786f5c23f1a1a5915207a34f719e2fe4
Drish-xD <drish.xd@gmail.com>
2025-01-01 15:34:46 +0530

    feat: update build script in package.json and add custom build script

---
+++ scripts/build.ts
@@ -0,0 +1,31 @@
+import Bun from "bun";
+import fs from "node:fs";
+import path from "node:path";
+
+// NodeJS Build
+const NODE_FIX = 'import { createRequire as createImportMetaRequire } from "module"; import.meta.require ||= (id) => createImportMetaRequire(import.meta.url)(id);\n';
+const BUILD_DIR = "../dist";
+
+try {
+  console.log("Building...");
+	const nodeBuild = await Bun.build({
+		entrypoints: ["./src/index.ts"],
+		target: "node",
+		minify: true,
+	});
+
+	// Write output files
+	for (const result of nodeBuild.outputs) {
+		const fileContent = NODE_FIX + (await result.text());
+		const destDir = path.join(import.meta.dir, BUILD_DIR);
+		const dest = path.join(destDir, result.path);
+		fs.existsSync(destDir) || fs.mkdirSync(destDir);
+		Bun.write(dest, fileContent);
+    console.log(`Wrote ${dest}`);
+	}
+  
+  console.log("Build successful");
+} catch (error) {
+	console.error("Build failed : ", error);
+	process.exit(1);
+}
