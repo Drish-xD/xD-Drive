6e75f53cca520f64cc8dff4e7489ed529a713bc8
Drish-xD <drish.xd@gmail.com>
2025-01-14 00:49:30 +0530

    chore: added schema utils

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,5 +1,8 @@
 export const AUTH = {
+	REGISTERED: "User registered successfully",
 	USER_ALREADY_EXISTS: "User already exists",
 	USER_NOT_FOUND: "User not found",
-	INVALID_CREDENTIALS: "Invalid email or password",
+	INVALID_CREDENTIALS: "Invalid credentials",
+	LOGGED_IN: "User successfully logged in!",
+	LOGGED_OUT: "User successfully logged out!",
 } as const;
