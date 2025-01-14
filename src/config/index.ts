a0f003b0b83a903eecbb9f131ca826b69817419d
Drish-xD <drish.xd@gmail.com>
2025-01-14 20:45:06 +0530

    chore: auth cookies

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,13 +9,15 @@ const strToBoolean = z.coerce
 const configSchema = z
 	.object({
 		APP_ENV: z.enum(["prod", "test", "dev"]).default("dev"),
+		BASE_URL: z.string().default("http://localhost:3000"),
 		IsProd: z.boolean().default(false),
 
 		// LOGGING
 		LOG_LEVEL: z.custom<LevelWithSilent>().default("info"),
 
 		// JWT
-		JWT_SECRET: z.string(),
+		JWT_SECRET: z.string().default("secret ingredient"),
+		COOKIE_SECRET: z.string().default("secret ingredient"),
 
 		// DATABASE
 		DATABASE_URL: z.string(),
