15f19632a8364f1f55cbbb85d554c9efcfb76e93
Drish-xD <drish.xd@gmail.com>
2025-01-16 18:44:09 +0530

    chore: users api

--- src/routes/users/users.index.ts
+++ src/routes/users/users.index.ts
@@ -2,6 +2,11 @@ import { createApp } from "@/helpers/app.helpers";
 import * as handlers from "./users.handlers";
 import * as routes from "./users.routes";
 
-const app = createApp().openapi(routes.me, handlers.me);
+const app = createApp()
+	.openapi(routes.me, handlers.me)
+	.openapi(routes.users, handlers.users)
+	.openapi(routes.user, handlers.user)
+	.openapi(routes.updateUser, handlers.updateUser)
+	.openapi(routes.deleteUser, handlers.deleteUser);
 
 export default app;
